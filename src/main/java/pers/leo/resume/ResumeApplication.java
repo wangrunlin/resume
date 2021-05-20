244a2840478c92e006ef7fa418afdb0fd15b2c66
Leo <alin.run@foxmail.com>
2021-05-20 23:27:30 +0800

    feat: init springboot application

---
+++ src/main/java/pers/leo/resume/ResumeApplication.java
@@ -0,0 +1,13 @@
+package pers.leo.resume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResumeApplication.class, args);
+    }
+
+}
