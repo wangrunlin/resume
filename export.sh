e669f5c4435b6b538d1f132d371db11d9f7ff99e
Leo Wang <leo@wangrunlin.com>
2023-06-16 01:41:15 +0800

    feat: update resume & add simple & upgrade theme

--- export.sh
+++ export.sh
@@ -1,3 +1,10 @@
+cp patch/*.patch yaml/
+
+for name in `ls yaml/*.patch`
+do
+  patch ${name%.patch}.yaml < $name
+done
+
 yaml2json --save yaml
 
 for name in `ls yaml/*.json`
@@ -5,6 +12,7 @@ do
   resume export ${name%.json}.html --resume $name --theme latte
 done
 
-rm yaml/*.json
-
 mv yaml/*.html out/
+
+rm yaml/*.json
+rm yaml/*.patch
