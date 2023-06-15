839cf8cac5ade2fbde70592912a82d18c3f4b923
Leo Wang <leo@wangrunlin.com>
2023-06-16 01:52:47 +0800

    feat: update patch and router

--- export.sh
+++ export.sh
@@ -1,3 +1,7 @@
+# copy simple yaml
+cp yaml/en.yaml yaml/simple-en.yaml
+cp yaml/zh.yaml yaml/simple-zh.yaml
+
 cp patch/*.patch yaml/
 
 for name in `ls yaml/*.patch`
@@ -14,5 +18,10 @@ done
 
 mv yaml/*.html out/
 
+# router
+cp out/en.html out/index.html
+cp out/simple-en.html out/simple.html
+
 rm yaml/*.json
 rm yaml/*.patch
+rm yaml/simple*
